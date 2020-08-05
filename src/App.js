56aed9ba4085a371d3a83667f3605c06f53ad82e
Blessing Krofegha <bkrofegha@gmail.com>
2020-08-05 11:29:04 +0200

    made intro text animate

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, {useEffect, useRef} from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import "./App.scss";
 import Header from "./components/Header";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-function App() {
+gsap.registerPlugin(ScrollTrigger);
+const App = () => {
+
   return (
     <Router>
       <div className='App'>
@@ -38,11 +42,59 @@ function Contact() {
 }
 
 function Home() {
+  //Create a variable for our dom nodes
+  let intro = useRef(null)
+  let scroll = useRef(null)
+  let wrapper = useRef(null)
+
+  useEffect(() => {
+    const textIntro = gsap.timeline();
+    textIntro
+      .from('.intro', {
+        xPercent: -20,
+        opacity: 0,
+        stagger: 0.2,
+        duration: 2
+      })
+  }, [])
   return (
     <div className='container'>
-      <div className='wrapper'>
-        <h5>
-          The <b>SHOPPER</b>, is a worldclass, innovative, global online ecommerce platform,
+      <div className='wrapper' ref={wrapper}>
+        <div className="hero__img"
+        ref={scroll}
+        >
+          {/* <div class="gallery">
+            <a target="_blank" href="/">
+              <img src={require("./images/img_5terre.jpg")} alt="Cinque Terre" width="600" height="400"/>
+            </a>
+          </div>
+
+            <div class="gallery">
+              <a target="_blank" href="/">
+              <img src={require("./images/img_forest.jpg")} alt="Forest" width="600" height="400"/>
+              </a>
+            </div>
+
+              <div class="gallery">
+                <a target="_blank" href="/">
+              <img src={require("./images/img_lights.jpg")} alt="Northern Lights" width="600" height="400"/>
+                </a>
+              </div>
+
+                <div class="gallery">
+                  <a target="_blank" href="/">
+              <img src={require("./images/img_mountains.jpg")} alt="Mountains" width="600" height="400"/>
+                  </a>
+                </div> */}
+          {/* <img className="hero__img--1" src={require("./images/cream.webp")} alt="cream"/>
+          <img className="hero__img--2" src={require("./images/drugs.webp")} alt="drugs"/>
+          <img className="hero__img--3" src={require("./images/pringles.webp")} alt="pringles"/>
+          <img className="hero__img--4" src={require("./images/provisions.webp")} alt="provisions"/> */}
+				</div>
+        <h5 className="intro"
+          ref={intro}
+        >
+          <b>SHOPPER</b>, is a worldclass, innovative, global online ecommerce platform,
           that meets your everyday daily needs.
         </h5>
       </div>
