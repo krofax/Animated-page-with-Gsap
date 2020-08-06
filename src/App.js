0f03b0ff909206200f5a0712e0e7118b996e1402
Blessing Krofegha <bkrofegha@gmail.com>
2020-08-06 13:49:53 +0200

    add useffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useRef, useEffect} from "react";
 import "./App.scss";
 import Header from "./components/Header";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
