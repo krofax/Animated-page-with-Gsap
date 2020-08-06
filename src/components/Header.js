1ffaaa7c93358e2360f6e62fb2e93b61d5f4c294
Blessing Krofegha <bkrofegha@gmail.com>
2020-08-06 14:40:51 +0200

    initial commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,72 +1,75 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { withRouter, Link } from "react-router-dom";
-
+import Menu from "./Menu";
+import { textIntro } from "./Animate";
 
 const Header = ({ history }) => {
+  let logo = useRef(null);
   // State of our Menu
   const [state, setState] = useState({
     initial: false,
     clicked: null,
-    menuName: "Menu"
+    menuName: "Menu",
   });
   // State of our button
   const [disabled, setDisabled] = useState(false);
 
-  //Use Effect
-  // useEffect(() => {
-  //   //Listening for page changes.
-  //   history.listen(() => {
-  //     setState({ clicked: false, menuName: "Menu" });
-  //   });
-  // }, [history]);
+  //When the component mounts
+  useEffect(() => {
+    textIntro(logo);
+    //Listening for page changes.
+    history.listen(() => {
+      setState({ clicked: false, menuName: "Menu" });
+    });
+  }, [history]);
 
-  // Toggle menu
-  // const handleMenu = () => {
-  //   disableMenu();
-  //   if (state.initial === false) {
-  //     setState({
-  //       initial: null,
-  //       clicked: true,
-  //       menuName: "Close"
-  //     });
-  //   } else if (state.clicked === true) {
-  //     setState({
-  //       clicked: !state.clicked,
-  //       menuName: "Menu"
-  //     });
-  //   } else if (state.clicked === false) {
-  //     setState({
-  //       clicked: !state.clicked,
-  //       menuName: "Close"
-  //     });
-  //   }
-  // };
+  //toggle menu
+  const handleMenu = () => {
+    disableMenu();
+    if (state.initial === false) {
+      setState({
+        initial: null,
+        clicked: true,
+        menuName: "Close",
+      });
+    } else if (state.clicked === true) {
+      setState({
+        clicked: !state.clicked,
+        menuName: "Menu",
+      });
+    } else if (state.clicked === false) {
+      setState({
+        clicked: !state.clicked,
+        menuName: "Close",
+      });
+    }
+  };
 
-  //Determine if out menu button should be disabled
-  // const disableMenu = () => {
-  //   setDisabled(!disabled);
-  //   setTimeout(() => {
-  //     setDisabled(false);
-  //   }, 1200);
-  // };
+  // check if out button is disabled
+  const disableMenu = () => {
+    setDisabled(!disabled);
+    setTimeout(() => {
+      setDisabled(false);
+    }, 1200);
+  };
 
   return (
     <header>
       <div className="container">
         <div className="wrapper">
           <div className="inner-header">
-            <div className="logo">
+            <div className="logo" ref={(el) => (logo = el)}>
               <Link to="/">SHOPPER.</Link>
             </div>
             <div className="menu">
-              <button>
+              <button disabled={disabled} onClick={handleMenu}>
                 {state.menuName}
               </button>
             </div>
           </div>
         </div>
       </div>
-      
+      <Menu state={state} />
     </header>
   );
 };
