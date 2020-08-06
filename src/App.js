b767987a0308279c4e07eb2d019a339749e0ddb9
Blessing Krofegha <bkrofegha@gmail.com>
2020-08-06 14:32:03 +0200

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React, {useEffect, useRef} from "react"
+import React, {useRef, useEffect} from "react";
 import "./App.scss";
-import Header from "./components/Header"
-import {Image} from "./components/Image"
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
-import { textIntro, skewGallery } from "./components/Animate"
+import Header from "./components/Header";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-const App = () => {
-
+function App() {
   return (
     <Router>
-      <div className="App">
+      <div className='App'>
         <Header />
-        <div className="container">
-          <div className="wrapper">
-            <div className="home">
+        <div className='container'>
+          <div className='wrapper'>
+            <div className='home'>
               <Switch>
-                <Route exact path="/" component={Home} />
-                <Route exact path="/about-us" component={About} />
-                <Route exact path="/contact-us" component={Contact} />
-                <Route exact path="/gallery" component={Gallery} />
+                <Route exact path='/' component={Home} />
+                <Route exact path='/about' component={About} />
+                <Route exact path='/services' component={Services} />
+                <Route exact path='/contact-us' component={Contact} />
               </Switch>
             </div>
           </div>
@@ -32,38 +29,21 @@ function About() {
   return <p>We have a decade years experience of worldclass services</p>;
 }
 
+function Services() {
+  return <p>We can take care of your home, office and your day to day needs.</p>;
+}
+
 function Contact() {
   return <p>Feel free to reach us.</p>;
 }
-function Gallery() {
-  let skewImage = useRef(null);
-  useEffect(() => {
-    skewGallery(skewImage)
-
-  }, []);
-  return (
-    <div ref={(el) => (skewImage = el)}>
-      <Image/>
-
-    </div>
-  )
-}
 
 function Home() {
-  //Create a variable for our dom nodes
-  let intro = useRef(null)
-
-  useEffect(() => {
-    //animate text
-    textIntro(intro)
-  }, [])
   return (
-    <div className="container">
-      <div className="wrapper">
-        <div className="hero__img"></div>
-        <h5 className="intro" ref={(el) => (intro = el)}>
-          <b>SHOPPER</b>, is a worldclass, innovative, global online ecommerce
-          platform, that meets your everyday daily needs.
+    <div className='container'>
+      <div className='wrapper'>
+        <h5 className="intro">
+          The <b>SHOPPER</b>, is a worldclass, innovative, global online ecommerce platform,
+          that meets your everyday daily needs.
         </h5>
       </div>
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,8 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import { withRouter, Link } from "react-router-dom";
-import Menu from "./Menu"
-import { textIntro } from "./Animate"
+
 
 const Header = ({ history }) => {
-
-  let logo = useRef(null)
   // State of our Menu
   const [state, setState] = useState({
     initial: false,
@@ -15,62 +12,61 @@ const Header = ({ history }) => {
   // State of our button
   const [disabled, setDisabled] = useState(false);
 
-  //When the component mounts
-  useEffect(() => {
-    textIntro(logo)
-    //Listening for page changes.
-    history.listen(() => {
-      setState({ clicked: false, menuName: "Menu" });
-    });
-  }, [history]);
+  //Use Effect
+  // useEffect(() => {
+  //   //Listening for page changes.
+  //   history.listen(() => {
+  //     setState({ clicked: false, menuName: "Menu" });
+  //   });
+  // }, [history]);
 
-  //toggle menu
-  const handleMenu = () => {
-    disableMenu();
-    if (state.initial === false) {
-      setState({
-        initial: null,
-        clicked: true,
-        menuName: "Close"
-      });
-    } else if (state.clicked === true) {
-      setState({
-        clicked: !state.clicked,
-        menuName: "Menu"
-      });
-    } else if (state.clicked === false) {
-      setState({
-        clicked: !state.clicked,
-        menuName: "Close"
-      });
-    }
-  };
+  // Toggle menu
+  // const handleMenu = () => {
+  //   disableMenu();
+  //   if (state.initial === false) {
+  //     setState({
+  //       initial: null,
+  //       clicked: true,
+  //       menuName: "Close"
+  //     });
+  //   } else if (state.clicked === true) {
+  //     setState({
+  //       clicked: !state.clicked,
+  //       menuName: "Menu"
+  //     });
+  //   } else if (state.clicked === false) {
+  //     setState({
+  //       clicked: !state.clicked,
+  //       menuName: "Close"
+  //     });
+  //   }
+  // };
 
-  // check if out button is disabled
-  const disableMenu = () => {
-    setDisabled(!disabled);
-    setTimeout(() => {
-      setDisabled(false);
-    }, 1200);
-  };
+  //Determine if out menu button should be disabled
+  // const disableMenu = () => {
+  //   setDisabled(!disabled);
+  //   setTimeout(() => {
+  //     setDisabled(false);
+  //   }, 1200);
+  // };
 
   return (
     <header>
       <div className="container">
         <div className="wrapper">
           <div className="inner-header">
-            <div className="logo" ref={(el) => (logo = el)}>
+            <div className="logo">
               <Link to="/">SHOPPER.</Link>
             </div>
             <div className="menu">
-              <button disabled={disabled} onClick={handleMenu}>
+              <button>
                 {state.menuName}
               </button>
             </div>
           </div>
         </div>
       </div>
-      <Menu state={state} />
+      
     </header>
   );
 };
